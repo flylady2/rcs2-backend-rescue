cacfde94709f4802b932c5273c04eef0cf4f1bc9
Leslie Stevens <flymaven@protonmail.com>
2021-01-16 13:58:49 -0600

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create, :show]
+      resources :surveys, only: [:create, :show, :destroy] do
+        resources :choices, only: [:new, :create]
+      end
+      resources :responses, only: [:new, :create, :destroy] do
+        resources :rankings, only: [:new, :create]
+      end
+      resources :rankings, only: [:update]
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
