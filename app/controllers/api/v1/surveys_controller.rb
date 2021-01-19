5cd745d1e0bac7c225242781f14fdddda8e3da42
Leslie Stevens <flymaven@protonmail.com>
2021-01-19 12:55:55 -0600

    modify surveys_controller

--- app/controllers/api/v1/surveys_controller.rb
+++ app/controllers/api/v1/surveys_controller.rb
@@ -8,6 +8,13 @@ class Api::V1::SurveysController < ApplicationController
     render json: { surveys: SurveySerializer.new(@surveys, options)}
   end
 
-  
+  def create
+    @survey = Survey.new(survey_params)
+  end
+
+  private
+    def survey_params
+      params.require(:survey).permit(:name, choices_attributes: [:content, :score, :survey_id])
+    end
 
 end
