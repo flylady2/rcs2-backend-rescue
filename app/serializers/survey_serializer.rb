2697dccbc19f9fd853849a162446bbbcca3a766a
Leslie Stevens <flymaven@protonmail.com>
2021-01-31 11:52:51 -0600

    add choices and responses as attributes in survey_serializer

--- app/serializers/survey_serializer.rb
+++ app/serializers/survey_serializer.rb
@@ -1,7 +1,7 @@
 class SurveySerializer
 
   include FastJsonapi::ObjectSerializer
-  attributes :name, :user_id
+  attributes :name, :user_id, :choices, :responses
   has_many :choices
   has_many :responses
 end
