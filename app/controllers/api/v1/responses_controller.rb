1f78202c14a1c25ac32ab55721e9833436da628a
Leslie Stevens <flymaven@protonmail.com>
2021-02-03 19:48:30 -0600

    add response create method

--- app/controllers/api/v1/responses_controller.rb
+++ app/controllers/api/v1/responses_controller.rb
@@ -1,8 +1,35 @@
 class Api::V1::ResponsesController < ApplicationController
 
+  def create
+    if params[:survey_id] && @survey = Survey.find_by_id(params[:survey_id].to_i)#nested uner responses
+      @response = @survey.responses.new(response_params)
+    end
+    @response.save
+    #byebug
+    if @response
+      @rankings = @response.rankings.build([{value: params["ranking1"].to_i, choice_id: params["ranking1_choiceId"].to_i}, {value: params["ranking2"].to_i, choice_id: params["ranking2_choiceId"].to_i}, {value: params["ranking3"].to_i, choice_id: params["ranking3_choiceId"].to_i}, {value: params["ranking4"].to_i, choice_id: params["ranking4_choiceId"].to_i}])
+
+
+      @rankings.each {|ranking|
+        ranking.save}
+      #byebug
+      options = {
+        include: [:rankings]
+      }
+
+      render json: ResponseSerializer.new(@response, options), status: :accepted
+
+    else
+      render json: { errors: @response.errors.full_messages}, status: :unprocessable_entity
+    end
+
+
+  end
+
+
 private
 
-def response_params
-  params.require(:response).permit(:respondent, :survey_id)
-
+  def response_params
+    params.require(:response).permit(:respondent, :survey_id, rankings_attributes: [:value, :response_id, :choice_id])
+  end
 end
