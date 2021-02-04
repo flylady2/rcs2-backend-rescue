4436fca2d64221406f22fb7b40e733d49c71d6d8
Leslie Stevens <flymaven@protonmail.com>
2021-02-03 19:54:44 -0600

    remove unique respondent validation

--- app/models/response.rb
+++ app/models/response.rb
@@ -3,6 +3,8 @@ class Response < ApplicationRecord
   belongs_to :survey
   has_many :rankings, dependent: :destroy
   has_many :choices, through: :rankings
-  validates :respondent, uniqueness: { case_sensitive: false}
+  #validates :respondent, uniqueness: { case_sensitive: false}
+
+  accepts_nested_attributes_for :rankings
 
 end
