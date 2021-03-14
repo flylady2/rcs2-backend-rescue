b11e0f81974b6cf2891e3c846f8fbf1b783fd703
Leslie Stevens <flymaven@protonmail.com>
2021-03-13 20:42:20 -0600

    change login params to email and password

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,8 +1,11 @@
 class Api::V1::AuthController < ApplicationController
   skip_before_action :authorized, only: [:create]
+  wrap_parameters :user, include: [:email, :password]
 
   def create
-    @user = User.find_by(username: user_login_params[:email])
+    #byebug
+    @user = User.find_by(email: user_login_params[:email])
+    #byebug
     if @user && @user.authenticate(user_login_params[:password])
       token = encode_token({ user_id: @user.id})
       render json: {user: UserSerializer.new(@user), jwt: token}, status: :accepted
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :surveys, dependent: :destroy
   has_many :responses, through: :surveys
   validates :username, uniqueness: { case_sensitive: false}
+  validates :email, uniqueness: { case_sensitive: false}
   has_secure_password
 
 end
